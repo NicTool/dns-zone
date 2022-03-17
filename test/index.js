834ec2c735e9ddbb5c1703ed44571d1e8d203ce7
Matt Simerson <matt@tnpi.net>
2022-03-17 15:28:08 -0700

    try adding --max-old-space-size to env

--- test/index.js
+++ test/index.js
@@ -6,35 +6,31 @@ const zv = require('../index')
 
 describe('parseZoneFile', function () {
 
-  it('parses a blank line', function (done) {
+  it('parses a blank line', async () => {
     const r = zv.parseZoneFile(`\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r, [])
-    done()
   })
 
-  it('parses two blank lines', function (done) {
+  it('parses two blank lines', async () => {
     const r = zv.parseZoneFile(`\n\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r, [])
-    done()
   })
 
-  it('parses a $TTL line', function (done) {
+  it('parses a $TTL line', async () => {
     const r = zv.parseZoneFile(`$TTL 86400\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r[0], { ttl: 86400 })
-    done()
   })
 
-  it('parses a $TTL line with a comment', function (done) {
+  it('parses a $TTL line with a comment', async () => {
     const r = zv.parseZoneFile(`$TTL 86400; yikers\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r[0], { ttl: 86400 })
-    done()
   })
 
-  it(`parses a SOA`, function (done) {
+  it(`parses a SOA`, async () => {
     const r = zv.parseZoneFile(`example.com.   86400   IN  SOA ns1.example.com.    hostmaster.example.com. (
                     2021102100    ; serial
                     16384   ; refresh
@@ -57,10 +53,9 @@ describe('parseZoneFile', function () {
       expire : 604800,
       minimum: 2560,
     })
-    done()
   })
 
-  it('parses a NS line', function (done) {
+  it('parses a NS line', async () => {
     const r = zv.parseZoneFile(`cadillac.net.   14400   IN  NS  ns1.cadillac.net.\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r[0], {
@@ -70,10 +65,9 @@ describe('parseZoneFile', function () {
       type : 'NS',
       dname: 'ns1.cadillac.net.',
     })
-    done()
   })
 
-  it('parses an A line', function (done) {
+  it('parses an A line', async () => {
     const r = zv.parseZoneFile(`cadillac.net.   86400   IN  A   66.128.51.173\n`)
     // console.dir(r, { depth: null })
     assert.deepStrictEqual(r[0], {
@@ -83,10 +77,9 @@ describe('parseZoneFile', function () {
       type   : 'A',
       address: '66.128.51.173',
     })
-    done()
   })
 
-  it('parses the cadillac.net zone file', function (done) {
+  it('parses the cadillac.net zone file', async () => {
     const file = './test/fixtures/zones/cadillac.net'
     fs.readFile(file, (err, buf) => {
       if (err) throw err
@@ -95,10 +88,9 @@ describe('parseZoneFile', function () {
       // console.dir(r, { depth: null })
       assert.equal(r.length, 41)
     })
-    done()
   })
 
-  it('parses the isi.edu zone file', function (done) {
+  it('parses the isi.edu zone file', async () => {
     const file = './test/fixtures/zones/isi.edu'
     fs.readFile(file, (err, buf) => {
       if (err) throw err
@@ -106,7 +98,6 @@ describe('parseZoneFile', function () {
       const r = zv.parseZoneFile(buf.toString())
       // console.dir(r, { depth: null })
       assert.equal(r.length, 11)
-      done()
     })
   })
 })
