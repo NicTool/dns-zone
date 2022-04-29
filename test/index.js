66608a6d976870fd7b8a444e58a1dadb2a3e424c
Matt Simerson <matt@tnpi.net>
2022-04-28 23:53:32 -0700

    feat(bind): convert 1d2h type ttls to seconds

--- test/index.js
+++ test/index.js
@@ -17,4 +17,23 @@ describe('dns-zone', function () {
       assert.strictEqual(dz.hasUnquoted('this is a string "of ( quoted" text', '"', '('), false)
     })
   })
+
+  const cases = {
+    '1w2d3h4m5s': 788645,
+    '1w1d'      : 691200,
+    '1w'        : 604800,
+    '1d'        : 86400,
+    '2h'        : 7200,
+    '1m'        : 60,
+    '3600'      : 3600,
+    '4500s'     : 4500,
+  }
+
+  describe('toSeconds', function () {
+    for (const c in cases) {
+      it(`converts ${c} to ${cases[c]} seconds`, function () {
+        assert.equal(dz.toSeconds(c), cases[c])
+      })
+    }
+  })
 })
